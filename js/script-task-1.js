c611ff98e5327df08f98f2c3a2b65529c389da5d
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-09-17 12:37:44 +0300

    20

---
+++ js/script-task-1.js
@@ -0,0 +1,25 @@
+/**
+ * We have a list of elements on html page
+ * 1. Print a text content of elements
+ * 2. Add element 'Coke'
+ * 2. Color text in list in red
+ */
+
+const elements = document.querySelectorAll("li");
+elements.forEach((el) => console.log(el.textContent));
+
+const li = document.createElement("li");
+
+// li.innerText = 'Coke';
+li.appendChild(document.createTextNode("Coke"));
+const parent = document.querySelector("ul");
+
+parent.append(li);
+// parent.appendChild(li);
+// parent.append('<li>Coke</li>');
+// parent.insertAdjacentHTML('beforeend', '<li>Coke</li>');
+// parent.insertAdjacentHTML('beforeend', li);
+
+document.querySelectorAll("li").forEach((el) => (el.style.color = "Red"));
+
+var t = 123;
---
+++ js/script-task-5.js
@@ -0,0 +1,21 @@
+/**
+ * Set class 'red-score' to score under 10
+ * Set class 'green-score' to other score
+ */
+
+const table = document.querySelector("table");
+
+const lines = table.querySelectorAll("td");
+lines.forEach((el) => {
+  const innerNumber = parseInt(el.innerText);
+  console.log(innerNumber, el.innerText);
+  if (!Number.isNaN(innerNumber)) {
+    if (innerNumber < 10) {
+      el.classList.add("red-score");
+    } else {
+      el.classList.add("green-score");
+    }
+  }
+});
+
+// покрасить в цвет еще и название страны. цвет такой же как и число.
